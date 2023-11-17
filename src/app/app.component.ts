470122a2e3d77ce5e524595f4e6d4603acff45c7
root <root@DESKTOP-LFCCF4J>
2023-11-17 17:10:00 -0500

    ciclo for reservas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reservacancha';
+  title = 'Reserva de canchitas';
+
+  tasks = [
+    'Escoge tu cancha',
+    'Elige la fecha',
+    'Datos personales',
+    'Forma de Pago',
+    'Reservado'
+  ]
+
 }
