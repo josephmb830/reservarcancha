f2b525b8cc370cca86f38fda2527f7203fb07e72
root <root@DESKTOP-LFCCF4J>
2023-11-17 17:38:03 -0500

    component routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LabsComponent } from './pages/labs/labs.component';
+
+const routes: Routes = [
+
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+      path: 'labs',
+      component: LabsComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
