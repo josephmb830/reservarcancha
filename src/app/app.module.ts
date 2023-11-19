76717c28408769f43070beb1b2d517f43e57e5a3
josephmb830 <josephmoran@duck.com>
2023-11-19 13:49:59 -0500

    formato a menu y selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LabsComponent } from './pages/labs/labs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LabsComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
